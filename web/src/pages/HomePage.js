fe2706286eea5d16b896a4774b17515fc60d7329
Jeison Fabian Sandoval Castaneda <jeison.sandoval@cadena.com.co>
2021-07-12 21:52:38 -0500

    Arquitectura Inicial - hola mundo

---
+++ web/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from 'react-dom';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+//import HomeLayout from '../components/layout/home-layout/HomeLayout';
+
+const HomePage = () => {
+  return (
+    <div>
+     Hola Mundo
+      
+    </div>
+  )
+};
+
+ReactDom.render(<HomePage />, document.getElementById('root'));
